cac46339148ea6726ab785d58ed650fce231a1da
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-04-15 23:44:19 +0100

    Adding the Parser contract

---
+++ src/Contracts/Parser.php
@@ -0,0 +1,37 @@
+<?php namespace Arcanedev\LaravelMarkdown\Contracts;
+
+/**
+ * Interface  Parser
+ *
+ * @package   Arcanedev\LaravelMarkdown\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface Parser
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Parses a markdown string to HTML.
+     *
+     * @param  string $content
+     *
+     * @return string
+     */
+    public function parse($content);
+
+    /**
+     * Start capturing output to be parsed.
+     *
+     * @return void
+     */
+    public function begin();
+
+    /**
+     * Stop capturing output, parse the string from markdown to HTML and return it.
+     *
+     * @return string
+     */
+    public function end();
+}
