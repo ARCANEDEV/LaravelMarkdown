84cb9940e0330afd6ece27a919c6b73e184b5ab5
ARCANEDEV <arcanedev.maroc@gmail.com>
2019-04-21 13:47:15 +0100

    Adding Laravel 5.8 support

--- tests/LaravelMarkdownServiceProviderTest.php
+++ tests/LaravelMarkdownServiceProviderTest.php
@@ -23,14 +23,14 @@ class LaravelMarkdownServiceProviderTest extends TestCase
      | -----------------------------------------------------------------
      */
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
         $this->provider = $this->app->getProvider(LaravelMarkdownServiceProvider::class);
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
         unset($this->provider);
 
